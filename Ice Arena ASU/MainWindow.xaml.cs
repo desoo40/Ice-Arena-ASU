5160682b54d2d8229a39fe6a0ad3f050549c9dff
Denis Sorokin <deso40@gmail.com>
2021-05-09 01:46:34 +0300

    connect list with listviw

--- Ice Arena ASU/MainWindow.xaml.cs
+++ Ice Arena ASU/MainWindow.xaml.cs
@@ -20,9 +20,38 @@ namespace Ice_Arena_ASU
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class Transaction
+        {
+            public string Name { get; set; }
+            public decimal Amount { get; set; }
+            public DateTime Date { get; set; }
+
+            public Transaction(string name, decimal amount, DateTime date)
+            {
+                Name = name;
+                Amount = amount;
+                Date = date;
+            }
+
+        }
+
+        public List<Transaction> Transactions { get; set; } 
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Transactions = new List<Transaction>();
+
+            var tr1 = new Transaction("Ice", 5000, DateTime.Today);
+            var tr2 = new Transaction("Skates", 25000, DateTime.Today);
+            var tr3 = new Transaction("Sticks", 53000, DateTime.Today);
+
+            Transactions.Add(tr1);
+            Transactions.Add(tr2);
+            Transactions.Add(tr3);
+
+            DataContext = this;
         }
     }
 }
